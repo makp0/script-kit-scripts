d280b3c87a4b2f064344019c078f5601f59b1957
Maksym Polupan <22623598+makp0@users.noreply.github.com>
2024-09-08 16:39:49 +0200

    add ssh poc

---
+++ attach-vscode-to-server-ssh.js
@@ -0,0 +1,45 @@
+// Name: Attach VSCode To Server SSH
+// Description: Launch scriptkit.com/tutorials in your browser
+// Author: John Lindquist
+// Twitter: @johnlindquist
+
+// Note: Feel free to delete this script!
+
+import "@johnlindquist/kit";
+import { Client } from 'hetzner-cloud-js';
+import { NodeSSH } from 'node-ssh';
+
+const apiKey = await env("HCLOUD_KEY");
+const client = new Client(apiKey);
+
+const servers = await client.servers.list();
+const host = await micro("Select server", servers.servers.map(server => {
+    return {
+        description: server.publicNet.ipv4.ip,
+        name: server.name,
+        value: server.publicNet.ipv4.ip
+    }
+} ));
+
+let ssh = new NodeSSH();
+await ssh.connect({
+    host: host,
+    username: 'root',
+    agent: process.env.SSH_AUTH_SOCK
+});
+
+// Execute command to read all users
+let result = await ssh.execCommand('cat /etc/passwd | cut -d: -f1');
+// await inspect(result);
+if (result.stderr) {
+    console.error('Error:', result.stderr);
+} else {
+    console.log('Users:', result.stdout);
+}
+
+// Disconnect from SSH
+ssh.dispose();
+
+await exec(`ssh-keygen -R "${host}"`);
+await exec(`/opt/homebrew/bin/code --folder-uri vscode-remote://ssh-remote+developer@${host}/home/developer`);
+
